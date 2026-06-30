8e4f138be96b5f077ec00424f7d462e6512e87e8
Vitor Táboas Fraga <vitortaboasfraga@gmail.com>
2026-06-30 10:08:33 +0200

    refactor(index): modularize bootstrap and error handling

--- commands/utility/playerManager.js
+++
@@ -1,2 +0,0 @@
-const {createAudioPlayer} = require('@discordjs/voice')
-
--- index.js
+++ index.js
@@ -29,60 +29,68 @@ async function setUpExtractors(){
 	}
 }
 
-setUpExtractors();
+process.on("unhandledRejection", (reason)=>{
+	const isAbort = 
+		reason?.name === 'AbortError' || 
+        reason?.code === 'ABORT_ERR' || 
+        reason?.cause?.name === 'AbortError';
 
-client.once(Events.ClientReady, (readyClient) => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+	if(isAbort){
 
-client.commands = new Collection()
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolder = fs.readdirSync(foldersPath)
-
-for(const folder of commandFolder){
-
-	const commandPath = path.join(foldersPath, folder)
-	const commandFile = fs.readdirSync(commandPath).filter((file)=>file.endsWith('js'))
-
-	for(const file of commandFile){
-		const filePath = path.join(commandPath, file)
-		const command = require(filePath)
-		if('data'in command && 'execute'in command){
-		client.commands.set(command.data.name, command)
-	} else{
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		
-	}
-	}	
-}
-
-client.login(TOKEN);
-
-client.on(Events.InteractionCreate, async (interaction) =>{
-
-	if(!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if(!command){
-		console.error(`No command matching ${interaction.commandName} was found.`);
+		console.warn('[Voice] Operação abortada (timeout ou desconexão):', reason.message);
 		return
 	}
 
-	try {
-		await command.execute(interaction);
-	} catch (err) {
-		if(interaction.replied || interaction.deferred){
-			await interaction.followUp({
-				content: 'There was an error while executing this command!',
-				flags: MessageFlags.Ephemeral,
-			});
+	console.error('[Unhandled Rejection]', reason);
+})
+
+
+async function bootStrap(){
+
+	setUpPlayerEvents(player)
+	await setUpExtractors();
+
+	client.commands = new Collection()
+
+	const foldersPath = path.join(__dirname, 'commands');
+	const commandFolder = fs.readdirSync(foldersPath)
+
+	for(const folder of commandFolder){
+
+		const commandPath = path.join(foldersPath, folder)
+		const commandFile = fs.readdirSync(commandPath).filter((file)=>file.endsWith('js'))
+
+		for(const file of commandFile){
+			const filePath = path.join(commandPath, file)
+			const command = require(filePath)
+			if('data'in command && 'execute'in command){
+
+				client.commands.set(command.data.name, command)
+			} else{
+				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+				
+			}
+		}	
+	}
+
+	const eventPath = path.join(__dirname, 'events');
+	const eventFolder = fs.readdirSync(eventPath);
+
+	for(const file of eventFolder){
+
+		const filePath = path.join(eventPath, file);
+		const event = require(filePath)
+
+		if(event.once){
+
+			client.once(event.name, (...args)=> event.execute(args))
 		}else{
-			await interaction.reply({
-				content: 'There was an error while executing this command!',
-				flags: MessageFlags.Ephemeral,
-			})
+
+			client.on(event.name, (...args)=> event.execute(args))
 		}
 	}
-	
-})
+
+	await client.login(TOKEN);
+}
+
+bootStrap()
